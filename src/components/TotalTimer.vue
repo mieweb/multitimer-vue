2b587361d32ef9fbb473e3e3194326b057d0ca64
trip-flip <thomasbaugher@outlook.com>
2023-01-10 09:58:24 -0500

    More style changes for consistency.

--- src/components/TotalTimer.vue
+++ src/components/TotalTimer.vue
@@ -2,11 +2,19 @@
 import { TimerSystem } from '../data/TimerSystem';
 </script>
 <template>
-	<div class="d-flex flex-column align-items-center pt-4 text-secondary">
+	<div
+		id="total-time"
+		class="d-flex flex-column align-items-center pt-4 text-secondary"
+	>
 		<h5>Total Time:</h5>
-		<h4 id="total-time">
+		<h4>
 			{{ TimerSystem.totalTime() }}
 		</h4>
 	</div>
 </template>
 <script lang="ts">export default {};</script>
+<style>
+#total-time * {
+	color: var(--foreground-alt) !important;
+}
+</style>
