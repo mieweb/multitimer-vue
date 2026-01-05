2e872ac1dabdbc87c87252eb22a0286e6dd1bb05
Thomas Baugher <tbaugher@mieweb.com>
2026-01-05 14:42:30 -0500

    Sync AddTimer bill-status list with TimerData; fix default billStatus

--- src/data/TimerData.ts
+++ src/data/TimerData.ts
@@ -1,76 +1,80 @@
-import HMS from './HMS';
-import { Settings } from './Settings';
-
+import HMS from "./HMS";
+import { Settings } from "./Settings";
 
 export type BillStatus =
-      "Billable Time"
-    | "Feature"
-    | "Goodwill-Leadership Approved"
-    | "Goodwill-Rep"
-    | "Holiday"
-    | "Internal Meeting"
-    | "Non-Billable Admin/Other(mgr approved only)"
-    | "Non-Billable-Bug/Defect"
-    | "Non-Billable-Training/Education"
-    | "Non-Billable-Research/Helpdesk"
-    | "PTO"
-    | "SOW Line Item"
+  | "Billable Time"
+  | "Feature"
+  | "Goodwill-Leadership Approved"
+  | "Goodwill-Rep"
+  | "Holiday"
+  | "Internal Meeting"
+  | "Non-Billable Admin/Other(mgr approved only)"
+  | "Non-Billable-Bug/Defect"
+  | "Non-Billable-Training/Education"
+  | "Non-Billable-Research/Helpdesk"
+  | "PTO"
+  | "SOW Line Item";
 export const billStatuses = [
-    "Non-Billable Admin/Other(mgr approved only)",
-    "Non-Billable-Bug/Defect",
-    "Non-Billable-Training/Education",
-    "Non-Billable-Research/Helpdesk",
-    "Billable Time",
-    "Feature",
-    "Goodwill-Leadership Approved",
-    "Goodwill-Rep",
-    "Holiday",
-    "Internal Meeting",
-    "PTO",
-    "SOW Line Item",
+  "Non-Billable Admin/Other(mgr approved only)",
+  "Non-Billable-Bug/Defect",
+  "Non-Billable-Training/Education",
+  "Non-Billable-Research/Helpdesk",
+  "Billable Time",
+  "Feature",
+  "Goodwill-Leadership Approved",
+  "Goodwill-Rep",
+  "Holiday",
+  "Internal Meeting",
+  "PTO",
+  "SOW Line Item",
 ];
 export function isBillStatus(bsString: string): bsString is BillStatus {
-	return billStatuses.includes(bsString);
+  return billStatuses.includes(bsString);
 }
 
-export type Activity = 'Acct Management'
-    | 'Administrative'
-    | 'DB Administration'
-    | 'Design'
-    | 'Development'
-    | 'Documentation'
-    | 'Forms'
-    | 'Implementation'
-    | 'Layouts'
-    | 'Meeting'
-    | 'Project Management'
-    | 'Research'
-    | 'Sales'
-    | 'System Administration'
-    | 'Training'
-    | 'Testing'
-    | 'Other'
-    | 'Helpdesk Support'
-    | 'Holiday'
+export type Activity =
+  | "Acct Management"
+  | "Administrative"
+  | "DB Administration"
+  | "Design"
+  | "Development"
+  | "Documentation"
+  | "Forms"
+  | "Implementation"
+  | "Layouts"
+  | "Meeting"
+  | "Project Management"
+  | "Research"
+  | "Sales"
+  | "System Administration"
+  | "Training"
+  | "Testing"
+  | "Other"
+  | "Helpdesk Support"
+  | "Holiday";
 
 export type TimerId = number;
 
 export type TimerData = {
-    issue: string,
-    title: string,
-    time: HMS,
-    link: string,
-    comment: string,
-    billStatus: BillStatus,
-    controlsHidden: boolean,
-	activity: Activity,
-	lastUsed: number,
+  issue: string;
+  title: string;
+  time: HMS;
+  link: string;
+  comment: string;
+  billStatus: BillStatus;
+  controlsHidden: boolean;
+  activity: Activity;
+  lastUsed: number;
 };
 
-export type FavoriteTimer = Omit<TimerData, 'time' | 'comment' | 'controlsHidden' | 'lastUsed'>;
+export type FavoriteTimer = Omit<
+  TimerData,
+  "time" | "comment" | "controlsHidden" | "lastUsed"
+>;
 
-export type RawTimerData = Omit<TimerData, 'time'>
-	& { time: { hours: number, minutes: number, seconds: number } };
+export type RawTimerData = Omit<TimerData, "time"> & {
+  time: { hours: number; minutes: number; seconds: number };
+};
 
 /**
  * General interface for working with form data across modals.
@@ -88,75 +92,81 @@ export type RawTimerData = Omit<TimerData, 'time'>
  * ```
  */
 export function rawToTimerData(form: Partial<RawTimerData>): TimerData {
-	const partial = {
-		...form,
-		time: HMS.fromHumanReadable(form.time?.hours, form.time?.minutes, form.time?.seconds)
-	} as Partial<TimerData>;
-	return partialToTimerData(partial);
+  const partial = {
+    ...form,
+    time: HMS.fromHumanReadable(
+      form.time?.hours,
+      form.time?.minutes,
+      form.time?.seconds
+    ),
+  } as Partial<TimerData>;
+  return partialToTimerData(partial);
 }
 
 export function timerDataToRaw(timerData: TimerData): RawTimerData {
-	return {
-		title: timerData.title,
-		issue: timerData.issue,
-		comment: timerData.comment,
-		link: timerData.link,
-		billStatus: timerData.billStatus,
-		activity: timerData.activity,
-		controlsHidden: timerData.controlsHidden,
-		lastUsed: timerData.lastUsed,
-		time: {
-			hours: timerData.time.getHours(),
-			minutes: timerData.time.getMinutes(),
-			seconds: timerData.time.getSeconds()
-		},
-	};
+  return {
+    title: timerData.title,
+    issue: timerData.issue,
+    comment: timerData.comment,
+    link: timerData.link,
+    billStatus: timerData.billStatus,
+    activity: timerData.activity,
+    controlsHidden: timerData.controlsHidden,
+    lastUsed: timerData.lastUsed,
+    time: {
+      hours: timerData.time.getHours(),
+      minutes: timerData.time.getMinutes(),
+      seconds: timerData.time.getSeconds(),
+    },
+  };
 }
 
 export function partialToTimerData(partial: Partial<TimerData>): TimerData {
-	const time = partial.time ? HMS.clone(partial.time) : new HMS();
-	return {
-		issue: partial.issue?.toString() ?? '', // .toString() because this ends up being a number
-		title: partial.title?.trim() ?? '',
-		time: time,
-		link: partial.link?.trim() ?? '',
-		comment: partial.comment?.trim() || '',
-		billStatus: partial.billStatus ?? 'Non-Billable' as BillStatus,
-		controlsHidden: Settings.hideControls,
-		activity: partial.activity ?? 'Development',
-		lastUsed: partial.lastUsed ?? Date.now()
-	};
+  const time = partial.time ? HMS.clone(partial.time) : new HMS();
+  return {
+    issue: partial.issue?.toString() ?? "", // .toString() because this ends up being a number
+    title: partial.title?.trim() ?? "",
+    time: time,
+    link: partial.link?.trim() ?? "",
+    comment: partial.comment?.trim() || "",
+    billStatus:
+      partial.billStatus ??
+      ("Non-Billable Admin/Other(mgr approved only)" as BillStatus),
+    controlsHidden: Settings.hideControls,
+    activity: partial.activity ?? "Development",
+    lastUsed: partial.lastUsed ?? Date.now(),
+  };
 }
 
 export const activities = [
-    { value: 36, activity: "Acct Management" },
-    { value: 9, activity: "Development" },
-    { value: 16, activity: "Implementation/Support" },
-    { value: 17, activity: "Project Management" },
-    { value: 33, activity: "Testing" },
-    { value: 2483, activity: "Holiday" },
-    { value: 31, activity: "Administrative" },
-    { value: 14, activity: "DB Administration" },
-    { value: 8, activity: "Design" },
-    { value: 12, activity: "Documentation" },
-    { value: 34, activity: "Forms" },
-    { value: 30, activity: "Layouts" },
-    { value: 10, activity: "Meeting" },
-    { value: 11, activity: "Research" },
-    { value: 35, activity: "Sales" },
-    { value: 13, activity: "System Administration" },
-    { value: 18, activity: "Training" },
-    { value: 19, activity: "Other" },
-    { value: 87, activity: "Helpdesk Support" },
-    { value: 2916, activity: "General Support/Admin" },
+  { value: 36, activity: "Acct Management" },
+  { value: 9, activity: "Development" },
+  { value: 16, activity: "Implementation/Support" },
+  { value: 17, activity: "Project Management" },
+  { value: 33, activity: "Testing" },
+  { value: 2483, activity: "Holiday" },
+  { value: 31, activity: "Administrative" },
+  { value: 14, activity: "DB Administration" },
+  { value: 8, activity: "Design" },
+  { value: 12, activity: "Documentation" },
+  { value: 34, activity: "Forms" },
+  { value: 30, activity: "Layouts" },
+  { value: 10, activity: "Meeting" },
+  { value: 11, activity: "Research" },
+  { value: 35, activity: "Sales" },
+  { value: 13, activity: "System Administration" },
+  { value: 18, activity: "Training" },
+  { value: 19, activity: "Other" },
+  { value: 87, activity: "Helpdesk Support" },
+  { value: 2916, activity: "General Support/Admin" },
 ];
 
 export function getActivityValue(activity: string): number {
-	for (const entry of activities) {
-		if (entry.activity === activity) {
-			return entry.value;
-		}
-	}
+  for (const entry of activities) {
+    if (entry.activity === activity) {
+      return entry.value;
+    }
+  }
 
-	throw new Error(`Activity with name '${activity}' does not exist`);
+  throw new Error(`Activity with name '${activity}' does not exist`);
 }
--- src/modals/ImportMeetings.vue
+++ src/modals/ImportMeetings.vue
@@ -1,184 +1,171 @@
 <script setup lang="ts">
-import ModalTemplate from '../components/ModalTemplate.vue';
-import type { Action } from '../components/ModalTemplate.vue';
-import HMS from '../data/HMS';
-import { BillStatus, RawTimerData, rawToTimerData } from '../data/TimerData';
-import { TimerSystem } from '../data/TimerSystem';
-import type { ModalData } from '../data/ModalHandler';
+import ModalTemplate from "../components/ModalTemplate.vue";
+import type { Action } from "../components/ModalTemplate.vue";
+import HMS from "../data/HMS";
+import {
+  BillStatus,
+  RawTimerData,
+  rawToTimerData,
+  billStatuses,
+} from "../data/TimerData";
+import { TimerSystem } from "../data/TimerSystem";
+import type { ModalData } from "../data/ModalHandler";
 
 const props = defineProps<{
-	modalData: ModalData
+  modalData: ModalData;
 }>();
 
-const formDataCollection = props.modalData.importMeetingData?.map(meeting => {
-	const startEndDifference = new Date(meeting.end).getTime() - new Date(meeting.start).getTime();
-	const time = HMS.fromSeconds(startEndDifference / 1000);
-	const timerForm: Omit<RawTimerData, 'controlsHidden' | 'lastUsed'> = {
-		issue: meeting.issue,
-		title: meeting.title,
-		time: {
-			hours: time.getHours(),
-			minutes: time.getMinutes(),
-			seconds: time.getSeconds()
-		},
-		billStatus: 'Non-Billable' as BillStatus,
-		comment: '',
-		link: meeting.link,
-		activity: 'Meeting'
-	};
+const formDataCollection =
+  props.modalData.importMeetingData?.map((meeting) => {
+    const startEndDifference =
+      new Date(meeting.end).getTime() - new Date(meeting.start).getTime();
+    const time = HMS.fromSeconds(startEndDifference / 1000);
+    const timerForm: Omit<RawTimerData, "controlsHidden" | "lastUsed"> = {
+      issue: meeting.issue,
+      title: meeting.title,
+      time: {
+        hours: time.getHours(),
+        minutes: time.getMinutes(),
+        seconds: time.getSeconds(),
+      },
+      billStatus: "Non-Billable Admin/Other(mgr approved only)" as BillStatus,
+      comment: "",
+      link: meeting.link,
+      activity: "Meeting",
+    };
 
-	return { timerData: timerForm, chosen: false };
-}) || [];
+    return { timerData: timerForm, chosen: false };
+  }) || [];
 const actions: Action[] = [
-	{
-		title: 'Create timer(s)',
-		action: () => {
-			TimerSystem.importOutlookMeetings(
-				formDataCollection
-					.filter(formData => formData.chosen)
-					.map(({ timerData }) => rawToTimerData(timerData))
-			);
-		},
-		closeModal: true,
-		classes: 'btn-primary',
-		disabled: formDataCollection.length ? false : true
-	}
+  {
+    title: "Create timer(s)",
+    action: () => {
+      TimerSystem.importOutlookMeetings(
+        formDataCollection
+          .filter((formData) => formData.chosen)
+          .map(({ timerData }) => rawToTimerData(timerData))
+      );
+    },
+    closeModal: true,
+    classes: "btn-primary",
+    disabled: formDataCollection.length ? false : true,
+  },
 ];
 
 const randomReaction = () => {
-	const reactions = [ '😖', '😯', '😴', 'ಠ_ಠ', '(╯°□°）╯︵ ┻━┻', '(⌐■_■)'];
-	const randomIndex = Math.floor(Math.random() * reactions.length);
-	return reactions[randomIndex];
+  const reactions = ["😖", "😯", "😴", "ಠ_ಠ", "(╯°□°）╯︵ ┻━┻", "(⌐■_■)"];
+  const randomIndex = Math.floor(Math.random() * reactions.length);
+  return reactions[randomIndex];
 };
 </script>
 <template>
-	<ModalTemplate
-		:actions="actions"
-		title="Import Meetings from Outlook"
-	>
-		<div v-if="formDataCollection.length === 0">
-			<p>No meeting timers could be found {{ randomReaction() }}</p>
-		</div>
-		<div
-			v-for="formData of formDataCollection"
-			v-else
-			:key="formData.timerData.issue"
-			class="detected-meeting"
-		>
-			<div class="form-floating mb-3">
-				<input
-					id="atm-issue"
-					v-model="formData.timerData.issue"
-					type="number"
-					class="form-control"
-					placeholder="issue"
-				>
-				<label
-					for="atm-issue"
-					class="form-label"
-				>Issue #</label>
-			</div>
-			<div class="form-floating mb-3">
-				<input
-					id="atm-title"
-					v-model.trim="formData.timerData.title"
-					type="text"
-					class="form-control"
-					placeholder="title"
-				>
-				<label
-					for="atm-title"
-					class="form-label"
-				>Timer Title</label>
-			</div>
-			<div class="input-group mb-3">
-				<input
-					id="atm-hours"
-					v-model="formData.timerData.time.hours"
-					type="number"
-					class="form-control"
-					placeholder="Hrs."
-				>
-				<span class="input-group-text">:</span>
-				<input
-					id="atm-minutes"
-					v-model="formData.timerData.time.minutes"
-					type="number"
-					class="form-control"
-					placeholder="Mins."
-				>
-				<span class="input-group-text">:</span>
-				<input
-					id="atm-seconds"
-					v-model="formData.timerData.time.seconds"
-					type="number"
-					class="form-control"
-					placeholder="Secs."
-				>
-			</div>
-			<div class="mb-3">
-				<label
-					for="atm-billable"
-					class="form-label"
-				>Timer Bill Status</label>
-				<select
-					id="atm-billable"
-					v-model="formData.timerData.billStatus"
-					class="form-select"
-				>
-					<option selected>
-						Non-Billable
-					</option>
-					<option>Billable Time</option>
-					<option>SOW Line Item</option>
-					<option>MIE Goodwill (non-billable)</option>
-				</select>
-			</div>
-			<div class="form-floating mb-3">
-				<input
-					id="atm-comment"
-					v-model.trim="formData.timerData.comment"
-					type="text"
-					class="form-control"
-					placeholder="comment"
-				>
-				<label
-					for="timer-comment"
-					class="form-label"
-				>Timer Comment</label>
-			</div>
-			<div class="form-floating mb-3">
-				<input
-					id="atm-link"
-					v-model.trim="formData.timerData.link"
-					type="text"
-					class="form-control"
-					placeholder="meeting link"
-				>
-				<label
-					for="timer-link"
-					class="form-label"
-				>Timer Meeting Link</label>
-			</div>
-			<div class="mb-3 form-check">
-				<label
-					class="form-check-label"
-					for="add-timer-cb"
-				>Add This Timer</label>
-				<input
-					v-model="formData.chosen"
-					type="checkbox"
-					class="form-check-input add-timer-cb"
-				>
-			</div>
-			<hr>
-		</div>
-	</ModalTemplate>
+  <ModalTemplate :actions="actions" title="Import Meetings from Outlook">
+    <div v-if="formDataCollection.length === 0">
+      <p>No meeting timers could be found {{ randomReaction() }}</p>
+    </div>
+    <div
+      v-for="formData of formDataCollection"
+      v-else
+      :key="formData.timerData.issue"
+      class="detected-meeting"
+    >
+      <div class="form-floating mb-3">
+        <input
+          id="atm-issue"
+          v-model="formData.timerData.issue"
+          type="number"
+          class="form-control"
+          placeholder="issue"
+        />
+        <label for="atm-issue" class="form-label">Issue #</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input
+          id="atm-title"
+          v-model.trim="formData.timerData.title"
+          type="text"
+          class="form-control"
+          placeholder="title"
+        />
+        <label for="atm-title" class="form-label">Timer Title</label>
+      </div>
+      <div class="input-group mb-3">
+        <input
+          id="atm-hours"
+          v-model="formData.timerData.time.hours"
+          type="number"
+          class="form-control"
+          placeholder="Hrs."
+        />
+        <span class="input-group-text">:</span>
+        <input
+          id="atm-minutes"
+          v-model="formData.timerData.time.minutes"
+          type="number"
+          class="form-control"
+          placeholder="Mins."
+        />
+        <span class="input-group-text">:</span>
+        <input
+          id="atm-seconds"
+          v-model="formData.timerData.time.seconds"
+          type="number"
+          class="form-control"
+          placeholder="Secs."
+        />
+      </div>
+      <div class="mb-3">
+        <label for="atm-billable" class="form-label">Timer Bill Status</label>
+        <select
+          id="atm-billable"
+          v-model="formData.timerData.billStatus"
+          class="form-select"
+        >
+          <option v-for="status in billStatuses" :key="status" :value="status">
+            {{ status }}
+          </option>
+        </select>
+      </div>
+      <div class="form-floating mb-3">
+        <input
+          id="atm-comment"
+          v-model.trim="formData.timerData.comment"
+          type="text"
+          class="form-control"
+          placeholder="comment"
+        />
+        <label for="timer-comment" class="form-label">Timer Comment</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input
+          id="atm-link"
+          v-model.trim="formData.timerData.link"
+          type="text"
+          class="form-control"
+          placeholder="meeting link"
+        />
+        <label for="timer-link" class="form-label">Timer Meeting Link</label>
+      </div>
+      <div class="mb-3 form-check">
+        <label class="form-check-label" for="add-timer-cb"
+          >Add This Timer</label
+        >
+        <input
+          v-model="formData.chosen"
+          type="checkbox"
+          class="form-check-input add-timer-cb"
+        />
+      </div>
+      <hr />
+    </div>
+  </ModalTemplate>
 </template>
-<script lang="ts">export default {};</script>
+<script lang="ts">
+export default {};
+</script>
 <style>
 .detected-meeting:nth-last-child(1) > hr {
-	display: none;
+  display: none;
 }
 </style>
 import Action from "../data/Action.1";
