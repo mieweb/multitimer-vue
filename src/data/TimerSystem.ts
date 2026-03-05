70a61f5fdbd8aacf5847cfb1a267fae4557dfcec
Dave Carlson <dcarlson@mieweb.com>
2026-03-05 11:00:13 -0500

    Updates to get in improvements: 63 - Add "Clear all Comments" button at top 58 - Open/Close all Arrows enhancement 55 - Make it possible for two timers to have the same ticket

--- src/data/TimerSystem.ts
+++ src/data/TimerSystem.ts
@@ -51,18 +51,18 @@ class _TimerSystem {
     withTime: false,
   };
 
-  public addTimer(timerData: TimerData): boolean {
-    if (this.issueExistsInTimerList(timerData.issue)) return false;
+  public addTimer(timerData: TimerData, force = false): boolean {
+    if (!force && this.issueExistsInTimerList(timerData.issue)) return false;
     const id = this.newId();
     this.timerList.unshift({ id, timer: reactive(timerData) });
     this.startTimer(id);
     return true;
   }
 
-  public splitTimer(timerData: TimerData): boolean {
-    if (this.issueExistsInTimerList(timerData.issue)) return false;
+  public splitTimer(timerData: TimerData, force = false): boolean {
+    if (!force && this.issueExistsInTimerList(timerData.issue)) return false;
     if (!this.activeTimer) {
-      return this.addTimer(timerData);
+      return this.addTimer(timerData, force);
     }
 
     const splitFromTimer = this.getTimerById(this.activeTimer);
@@ -72,6 +72,10 @@ class _TimerSystem {
     this.timerList.unshift({ id, timer: reactive(timerData) });
     this.startTimer(id);
     return true;
+  }
+
+  public issueExists(issue: string): boolean {
+    return !!this.issueExistsInTimerList(issue);
   }
 
   /**
@@ -345,6 +349,30 @@ class _TimerSystem {
   public removeAllTimers() {
     this.timerList.splice(0, this.timerList.length);
     this.activeTimer = NaN;
+  }
+
+  public clearAllComments() {
+    for (const { timer } of this.timerList) {
+      timer.comment = "";
+    }
+  }
+
+  public toggleAllControls() {
+    if (this.timerList.length === 0) return;
+    
+    // Get the first timer's state to determine what to do
+    const firstTimer = this.timerList[0].timer;
+    const targetState = !firstTimer.controlsHidden;
+    
+    // Set all timers to the opposite of the first timer's current state
+    for (const { timer } of this.timerList) {
+      timer.controlsHidden = targetState;
+    }
+  }
+
+  public areAllControlsHidden(): boolean {
+    if (this.timerList.length === 0) return false;
+    return this.timerList[0].timer.controlsHidden;
   }
 
   public isLoggable(timer: TimerData) {
---
+++ src/modals/ClearAllComments.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import ModalTemplate from '../components/ModalTemplate.vue';
+import { TimerSystem } from '../data/TimerSystem';
+import type { Action } from '../components/ModalTemplate.vue';
+const actions: Action[] = [
+	{
+		title: 'Clear All Comments',
+		action: () => {
+			TimerSystem.clearAllComments();
+		},
+		closeModal: true,
+		classes: 'btn-danger',
+		hotkey: 'Enter'
+	}
+];
+
+</script>
+<template>
+	<ModalTemplate
+		:actions="actions"
+		:title="'Clear All Comments?'"
+	>
+		<div class="modal-body">
+			<label class="form-label">Are you sure you want to clear all comment fields?</label>
+		</div>
+	</ModalTemplate>
+</template>
+<script lang="ts">export default {};</script>
